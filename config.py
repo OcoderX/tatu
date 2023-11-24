07311076cc2e563db813e33725448939868e06c4
ocoderx <yt102523@gmail.com>
2023-11-24 10:18:53 +0500

    gd

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
 #Ushbu faylga Admin id va hashni my.telegram.org dan olib qo'ying
+API_IDd = '1784495'#buni o'rniga o'zinngiznikini ID
+API_HASHd = 'c9dsbhbsbb7b0f5dfbe725dabb388c82cb0d42' #buni o'rniga o'zingizni Hash
+bot_tok = '5223232324231:AAEoifpMSJKQWfjdkRaAogTb96aHK4yMBOSmsM' #Bot tokeningiz
